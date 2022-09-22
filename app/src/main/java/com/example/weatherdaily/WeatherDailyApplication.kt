111d92cd8aa7a9211ad87722701e5504f70cd05c
thaonguyenhuu251 <htnguyendev@gmail.com>
2022-09-22 20:14:35 +0700

    commit base first

---
+++ app/src/main/java/com/example/weatherdaily/WeatherDailyApplication.kt
@@ -0,0 +1,7 @@
+package com.example.weatherdaily
+
+import android.app.Application
+
+class WeatherDailyApplication : Application() {
+
+}
---
+++ app/src/main/java/com/example/weatherdaily/view/home/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.example.weatherdaily.view.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.weatherdaily.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
